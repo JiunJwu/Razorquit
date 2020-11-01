848b7c61f46ed7b42dae22854c151052622d7579
JiunJwu <pd4058@yahoo.com.tw>
2020-11-01 10:00:31 +0800

    加入專案檔案。

---
+++ razortest/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace razortest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Razor()
+        {
+            ViewBag.data = new int[] { 1, 2, 3, 4, 5 };
+            return PartialView();
+        }
+    }
+}
---
+++ razortest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="razortest.MvcApplication" Language="C#" %>
